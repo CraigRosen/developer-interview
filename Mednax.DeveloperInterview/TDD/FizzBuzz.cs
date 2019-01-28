3a9a44d738847245d5e7388ac055f168d6bf7fdf
tdorsey <tdorsey@users.noreply.github.com>
2019-01-28 11:47:26 -0500

    Initial submission

--- Mednax.DeveloperInterview/TDD/FizzBuzz.cs
+++ Mednax.DeveloperInterview/TDD/FizzBuzz.cs
@@ -1,15 +1,40 @@
-﻿using System;
+﻿//using System;
 
-namespace Mednax.DeveloperInterview.TDD
-{
-    /// <summary>
-    /// Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz"
-    /// instead of the number and for the multiples of five print "Buzz". For numbers which are multiples
-    /// of both three and five print "FizzBuzz".
-    /// i.e.:  "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 ..."
-    /// </summary>
-    public class FizzBuzz
-    {
+//namespace Mednax.DeveloperInterview.TDD
+//{
+//    /// <summary>
+//    /// Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz"
+//    /// instead of the number and for the multiples of five print "Buzz". For numbers which are multiples
+//    /// of both three and five print "FizzBuzz".
+//    /// i.e.:  "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 ..."
+//    /// </summary>
+//    public class FizzBuzz
+//    {
+//        private Boolean MultipleOfThree(int input) {
+//            return input % 3 == 0;
+//        }
+//        private Boolean MultipleOfFive() {
+//            return input % 5 == 0;
+//        }
 
-    }
-}
+//        public void FizzBuzz() {
+
+//            for (int i = 1; i <= 100; i++) {
+//                if (MultipleOfThree()) {
+//                    Console.Write("Fizz");
+//                }
+
+//                if (MultipleOfFive()) {
+//                    Console.Write("Buzz");
+//                }
+
+//                if (MultipleOfThree() && MultipleOfFive()) {
+//                    Console.Write("FizzBuzz");
+//                }
+
+                
+//            }
+
+//        }
+//    }
+//}
