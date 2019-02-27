f34c9002e8cad449ef2330e0f29c35ee0b8dfdf7
BrianRoys <brian@brianroys.com>
2019-02-27 11:57:26 -0500

    Brian Roys -- Times up; finished the last two exercises first, nothing in the TDD project.

--- Mednax.DeveloperInterview.Test/FizzBuzzUnitTests.cs
+++ Mednax.DeveloperInterview.Test/FizzBuzzUnitTests.cs
@@ -3,8 +3,10 @@ using Mednax.DeveloperInterview.TDD;
 
 namespace Tests
 {
+    [TestFixture]
     public class FizzBuzzUnitTests
     {
+        public void FizzBuzz_
        
     }
 }
--- Mednax.DeveloperInterview/TDD/FizzBuzz.cs
+++ Mednax.DeveloperInterview/TDD/FizzBuzz.cs
@@ -12,4 +12,9 @@ namespace Mednax.DeveloperInterview.TDD
     {
 
     }
+
+    public class FizzBuzzTests
+    {
+
+    }
 }
